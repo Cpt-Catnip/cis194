de072263c1fab64a118f9b144c8f7af34e38c5d9
Michael Koshakow <mzkosh@gmail.com>
2023-04-30 15:58:35 -0400

    exercise 1

--- week01/homework/validateCard.hs
+++ week01/homework/validateCard.hs
@@ -1,7 +1,9 @@
 -- file: week01/homework/validateCard.hs
 
 toDigits :: Integer -> [Integer]
-toDigits x = []
+toDigits x = reverse (toDigitsRev x)
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = []
+toDigitsRev x
+  | x <= 0 = []
+  | otherwise = mod x 10 : toDigitsRev (div x 10)
