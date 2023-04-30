23a9dd6519b3733619fafbcf86406eba98a30fb3
Michael Koshakow <mzkosh@gmail.com>
2023-04-30 16:17:44 -0400

    problem 2

--- week01/homework/validateCard.hs
+++ week01/homework/validateCard.hs
@@ -7,3 +7,11 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev x
   | x <= 0 = []
   | otherwise = mod x 10 : toDigitsRev (div x 10)
+
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' [] = []
+doubleEveryOther' [x] = [x]
+doubleEveryOther' (x : y : zs) = x : 2 * y : doubleEveryOther' zs
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = reverse (doubleEveryOther' (reverse xs))
