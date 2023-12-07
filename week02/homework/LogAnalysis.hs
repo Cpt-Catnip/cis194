0f3150dd55e33df526f4b051acd9374a01d1164e
Mike Koshakow <mzkosh@gmail.com>
2023-12-07 11:09:33 -0500

    scratch the surface of HW 2

---
+++ week02/homework/LogAnalysis.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+parseMessageList :: [String] -> LogMessage
+parseMessageList
+
+parseMessage :: String -> LogMessage
+parseMessage m@"E":xs = 
+
+parse :: String -> [LogMessage]
